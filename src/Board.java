c4fc987e1b9caf59899dfb0f23c9e854d5a1d413
jacob Cedar <jcedar8640@bths.edu>
2018-11-05 11:06:16 -0500

    added board?

--- src/Board.java
+++ src/Board.java
@@ -3,5 +3,12 @@ import People.Person;
 
 
 public class Board {
+    Room[][] map;
 
+    public Board(Room[][] map){
+        this.map = map;
+    }
+    public Board(int height, int width){
+        map = new Room[height][width];
+    }
 }
